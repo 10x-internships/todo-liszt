e3a251ea87873df01128f59161b1299700b69844
Nguyên Trần <nguyentranthepog@gmail.com>
2021-08-02 11:44:11 +0700

    feat: add Avatar component

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,5 @@
 import Head from 'next/head';
+import Avatar from '@components/Avatar';
 
 export default function Home() {
   return (
@@ -8,7 +9,18 @@ export default function Home() {
         <meta name="description" content="The best app for writing todos" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <h1>Todo Liszt</h1>
+      <Avatar
+        src="https://images.unsplash.com/photo-1627610939070-08af6fb83c04?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=701&q=80"
+        alt="avatar"
+        width={48}
+        height={48}
+      />
+      <Avatar
+        src="https://images.unsplash.com/photo-1627610939070-08af6fb83c04?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=701&q=80"
+        alt="avatar"
+        width={198}
+        height={198}
+      />
     </>
   );
 }
