5702ea720cb2567d6e60b47b6f8936ae506e3a54
Nguyên Trần <nguyentranthepog@gmail.com>
2021-08-09 17:11:56 +0700

    feat: add state for UI

--- redux/reducers/index.ts
+++ redux/reducers/index.ts
@@ -1,7 +1,8 @@
 import { combineReducers } from 'redux';
 
 import toastsReducer from './toasts';
+import uiReducers from './ui';
 
-const rootReducer = combineReducers({ toasts: toastsReducer });
+const rootReducer = combineReducers({ toasts: toastsReducer, ui: uiReducers });
 
 export default rootReducer;
