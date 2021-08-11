d6451311ef404e49405bff215c9a68247fecd249
Nguyên Trần <nguyentranthepog@gmail.com>
2021-08-11 17:06:11 +0700

    feat: add Icon for create list page

--- components/Icons/Circle.tsx
+++ components/Icons/Circle.tsx
@@ -1,13 +1,22 @@
-export const CircleLine = (props: React.SVGProps<SVGSVGElement>) => (
+interface CircleLineProps extends React.SVGProps<SVGSVGElement> {
+  circleFill?: string;
+}
+
+export const CircleLine = ({ circleFill, ...others }: CircleLineProps) => (
   <svg
     width={24}
     height={24}
     viewBox="0 0 24 24"
     fill="none"
     xmlns="http://www.w3.org/2000/svg"
-    {...props}
+    {...others}
   >
-    <path d="M21 12a9 9 0 11-18 0 9 9 0 0118 0z" stroke="currentColor" strokeWidth={2} />
+    <path
+      d="M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
+      fill={circleFill}
+      stroke="#777E91"
+      strokeWidth={2}
+    />
   </svg>
 );
 
