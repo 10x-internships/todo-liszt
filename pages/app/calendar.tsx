d381e565277d6eca06e96ab56fdb28738017ce2f
Nguyên Trần <nguyentranthepog@gmail.com>
2021-08-16 16:42:23 +0700

    feat: add calendar page

--- pages/app/calendar.tsx
+++ pages/app/calendar.tsx
@@ -1,26 +1,21 @@
 import Head from 'next/head';
-import dayjs from 'dayjs';
 
 import { DashboardLayout } from '@components/Layout';
 import { ListHeader } from '@fragments/app/List';
-import { CalendarHeader, CalendarTable } from '@fragments/app/Calendar';
+import Calendar from '@fragments/app/Calendar';
 
-export default function Calendar() {
-  const start = dayjs().startOf('month');
-  const end = dayjs().endOf('month');
-  console.log(start.get('day'), end.get('day'));
-
+export default function CalendarPage() {
   return (
     <>
       <Head>
         <title>Calendar | Todo Liszt</title>
       </Head>
+
       <ListHeader title="Calendar" />
-      <CalendarHeader />
-      <CalendarTable />
+      <Calendar />
     </>
   );
 }
 
 // eslint-disable-next-line react/display-name
-Calendar.getLayout = (page: React.ReactNode) => <DashboardLayout>{page}</DashboardLayout>;
+CalendarPage.getLayout = (page: React.ReactNode) => <DashboardLayout>{page}</DashboardLayout>;
