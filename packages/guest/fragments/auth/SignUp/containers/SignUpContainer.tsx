2d04aa71e205331a03286a7d8de4b481222eff49
Nguyên Trần <nguyentranthepog@gmail.com>
2021-08-30 16:56:44 +0700

    feat: add sign up function

--- packages/guest/fragments/auth/SignUp/containers/SignUpContainer.tsx
+++ packages/guest/fragments/auth/SignUp/containers/SignUpContainer.tsx
@@ -1,22 +1,32 @@
-import { SubmitHandler } from 'react-hook-form';
-import { useDispatch, useSelector } from 'react-redux';
+import { useEffect } from 'react';
+import { AxiosError } from 'axios';
 import { useRouter } from 'next/router';
+import { useMutation } from 'react-query';
+import { useDispatch } from 'react-redux';
+import { SubmitHandler } from 'react-hook-form';
 
-import { signUp } from '@redux/actions/auth';
-import { selectErrorMessage, selectIsLoading } from '@redux/selectors/auth';
+import { useToast } from '@todo-liszt/common';
+import todoLisztAPI from '@config/api';
 
 import AuthLogo from '../../components/AuthLogo';
 import AuthTitle from '../../components/AuthTitle';
 import AuthHaveAccount from '../../components/AuthHaveAccount';
 
-import SignUpForm from '../components/SignUpForm';
 import { IFormInput } from '../types';
+import SignUpForm from '../components/SignUpForm';
+import { updateData } from '@redux/actions/auth';
+import Cookies from 'js-cookie';
 
 const SignUpContainer = () => {
-  const router = useRouter();
   const dispatch = useDispatch();
-  const isLoading = useSelector(selectIsLoading);
-  const errorMessage = useSelector(selectErrorMessage);
+  const router = useRouter();
+  const toast = useToast();
+
+  const { isLoading, isSuccess, mutate, data: signUpData, error } = useMutation(
+    (newUser: { email: string; password: string }) => {
+      return todoLisztAPI.post('/users', newUser);
+    }
+  );
 
   const onSubmit: SubmitHandler<IFormInput> = (data) => {
     const signUpData = {
@@ -24,14 +34,24 @@ const SignUpContainer = () => {
       password: data.password,
     };
 
-    dispatch(signUp(signUpData, router));
+    mutate(signUpData);
   };
+
+  useEffect(() => {
+    if (isSuccess) {
+      toast?.addToast({ message: 'Sign up successfully', state: 'success' });
+      dispatch(updateData(signUpData?.data.data));
+      Cookies.set('userData', JSON.stringify(signUpData?.data.data));
+      router.push('/signin');
+    }
+    // eslint-disable-next-line
+  }, [isSuccess, router]);
 
   return (
     <>
       <AuthLogo />
       <AuthTitle>Sign up</AuthTitle>
-      <SignUpForm onSubmit={onSubmit} isLoading={isLoading} errorMessage={errorMessage} />
+      <SignUpForm onSubmit={onSubmit} isLoading={isLoading} error={error as AxiosError} />
       <AuthHaveAccount />
     </>
   );
