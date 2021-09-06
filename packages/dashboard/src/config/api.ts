f574358be3c340f244e8d2139f12a5e9c584cff9
Nguyên Trần <nguyentranthepog@gmail.com>
2021-09-06 16:48:54 +0700

    feat: add axios interceptor

--- packages/dashboard/src/config/api.ts
+++ packages/dashboard/src/config/api.ts
@@ -1,4 +1,5 @@
 import axios from "axios";
+import * as auth from "@services/auth";
 
 const todoLisztAPI = axios.create({
   baseURL: "http://api-todo-liszt.sestud.io/api/v1.0/",
@@ -6,5 +7,39 @@ const todoLisztAPI = axios.create({
     "Access-Control-Allow-Origin": "*",
   },
 });
+
+todoLisztAPI.interceptors.request.use(
+  (config) => {
+    const accessToken = auth.getAccessToken();
+
+    if (accessToken) {
+      config.headers["Authorization"] = accessToken;
+    }
+
+    return config;
+  },
+  (error) => Promise.reject(error)
+);
+
+todoLisztAPI.interceptors.response.use(
+  (response) => response,
+  (error) => {
+    const originalRequest = error.config;
+    const refreshToken = auth.getRefreshToken();
+    if (error.response.status === 401) {
+      todoLisztAPI
+        .post("/sessions/refresh", { refreshToken })
+        .then((res) => {
+          const { accessToken, refreshToken } = res.data;
+          auth.setAccessToken(accessToken);
+          auth.setRefreshToken(refreshToken);
+          todoLisztAPI.defaults.headers.common["Authorization"] = accessToken;
+          return todoLisztAPI(originalRequest);
+        })
+        .catch((error) => Promise.reject(error));
+    }
+    return Promise.reject(error);
+  }
+);
 
 export default todoLisztAPI;
