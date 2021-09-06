90db5cc537138623905ce8dfd49ae13c4575028d
Nguyên Trần <nguyentranthepog@gmail.com>
2021-09-06 16:53:21 +0700

    feat(Modal): add animation

--- packages/shared/common/src/components/Dropdown/index.ts
+++
@@ -1,3 +0,0 @@
-export { default as Dropdown, DropdownWrapper } from "./Dropdown";
-export { default as DropdownList } from "./DropdownList";
-export { default as DropdownItem } from "./DropdownItem";
