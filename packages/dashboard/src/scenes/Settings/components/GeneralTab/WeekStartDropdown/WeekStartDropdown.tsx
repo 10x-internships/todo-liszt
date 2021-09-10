e52a86fcf7faae5e0f5613f93898340d9ea74d14
Nguyên Trần <nguyentranthepog@gmail.com>
2021-09-10 17:34:25 +0700

    feat: add more days for weekstart

--- packages/dashboard/src/scenes/Settings/components/GeneralTab/WeekStartDropdown/WeekStartDropdown.tsx
+++ packages/dashboard/src/scenes/Settings/components/GeneralTab/WeekStartDropdown/WeekStartDropdown.tsx
@@ -6,14 +6,29 @@ import { setWeekStart } from '@redux/actions/settings';
 import { selectWeekStart } from '@redux/selectors/settings';
 
 const dayOptions = [
-  { id: 0, name: 'scene.Settings.AppSettings.WeekStart.monday', value: 'monday' },
-  { id: 1, name: 'scene.Settings.AppSettings.WeekStart.sunday', value: 'sunday' },
+  { id: 0, name: 'scene.Settings.AppSettings.WeekStart.sunday', value: 'sunday' },
+  { id: 1, name: 'scene.Settings.AppSettings.WeekStart.monday', value: 'monday' },
+  { id: 2, name: 'scene.Settings.AppSettings.WeekStart.tuesday', value: 'tuesday' },
+  { id: 3, name: 'scene.Settings.AppSettings.WeekStart.wednesday', value: 'wednesday' },
+  { id: 4, name: 'scene.Settings.AppSettings.WeekStart.thursday', value: 'thursday' },
+  { id: 5, name: 'scene.Settings.AppSettings.WeekStart.friday', value: 'friday' },
+  { id: 6, name: 'scene.Settings.AppSettings.WeekStart.saturday', value: 'saturday' },
 ];
 
 const getWeekStart = (day: string) => {
   switch (day) {
     case 'monday':
       return 'scene.Settings.AppSettings.WeekStart.monday';
+    case 'tuesday':
+      return 'scene.Settings.AppSettings.WeekStart.tuesday';
+    case 'wednesday':
+      return 'scene.Settings.AppSettings.WeekStart.wednesday';
+    case 'thursday':
+      return 'scene.Settings.AppSettings.WeekStart.thursday';
+    case 'friday':
+      return 'scene.Settings.AppSettings.WeekStart.friday';
+    case 'saturday':
+      return 'scene.Settings.AppSettings.WeekStart.saturday';
     case 'sunday':
       return 'scene.Settings.AppSettings.WeekStart.sunday';
     default:
